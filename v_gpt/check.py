40e36b491f74b361ac3241b7b2c560b5372998bb
Jay Maier <jamesmai@andre.cmu.edu>
2024-04-17 15:43:12 -0400

    new_changes

--- v_gpt/check.py
+++ v_gpt/check.py
@@ -6,10 +6,10 @@ import ipdb
 
 video_filename = '/home/jay/VPT/data/SegTrackv2/mp4s/girl.mp4'
 sequence_length = 16
-resolution = 128
+resolution = 64
 device = torch.device('cuda')
 
-vqvae = load_vqvae('kinetics_stride2x4x4')
+vqvae = load_vqvae('bair_stride4x2x2')
 video = read_video(video_filename, pts_unit='sec')[0]
 ipdb.set_trace()
 video = preprocess(video.cuda(), resolution, sequence_length).unsqueeze(0).to(device)
